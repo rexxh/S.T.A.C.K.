28e9971dad6ec6893a1611740947638965f94534
rexxh <rexxh@mail.ru>
2016-09-19 17:10:49 +0400

    Update Stack.hpp

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -52,11 +52,7 @@ void stack<T>::push(T const &obj) {
 	if (count_ +1 > array_size_)
 	{
 		array_size_ *= MULTIPLIER;
-		T * temp = new T[array_size_];
-		for (int i = 0; i < count_; i++)
-		{
-			temp[i] = array_[i];
-		}
+		T * temp = New_n_copy(array_size_, count_, array_);
 		delete[] array_;
 		array_ = temp;
 		std::cout << "\n ++++ Memory! ++++";
@@ -67,33 +63,25 @@ void stack<T>::push(T const &obj) {
 
 template <typename T>
 T stack<T>::pop() {
-	if ( count_ == 0) {
-		throw "nooooo!";
-	}
-	else {
+	if ( count_ > 0) 
+	{
 		T top = array_[count_ - 1];
 		--count_;
 		if ((array_size_ / count_) > MULTIPLIER)
 		{
 			array_size_ /= 1.5;
-			T* temp = new T[array_size_];
-			for (int i = 0; i < count_; i++)
-			{
-				temp[i] = array_[i];
-			}
+			T* temp = New_n_copy(array_size_, count_, array_);
 			delete[] array_; std::cout << "---- Memory! ----";
 			array_ = temp;
 		}
 		return top;
 	}
+	throw "nooooo!";
 }
 
 template <typename T>
 stack<T>::stack(const stack<T>& obj) : array_size_(obj.array_size_), count_(obj.count_){
-	array_ = new T[array_size_];
-	for (int i = 0; i < count_; i++) {
-	array_[i] = obj.array_[i];
-	}
+	array_ = New_n_copy(array_size_, count_, obj.array_);
 }
 
 template<typename T>
@@ -117,4 +105,12 @@ auto stack<T>::operator==(const stack & object) const -> bool
 		}
 	}
 	return true;
+}
+
+
+template<typename T>
+T* New_n_copy(size_t ar_size, size_t count_, T* ar_){ 
+	T*temp = new T[ar_size];
+	std::copy(ar_, ar_+count_, temp);
+	return temp;
 }
